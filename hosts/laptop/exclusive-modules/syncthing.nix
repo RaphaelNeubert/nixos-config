e227d61b54e507c5aca20db2bed90db74341c830
Raphael Neubert <raphi.neubert@gmail.com>
2024-11-08 22:12:58 +0100

    syncthing add desktop gentoo, documents

--- hosts/laptop/exclusive-modules/syncthing.nix
+++ hosts/laptop/exclusive-modules/syncthing.nix
@@ -19,11 +19,25 @@
         "desktop" = {
           id = "T33LOQG-DU3JDXZ-TAAVCFE-TWQRWJN-TYYOUVC-FOUUUJA-HWKDXWB-GVXKDAX";
         };
+        "desktop-gentoo" = {
+          id = "KFUAMOW-PU22UBP-BWA5H4Z-WHIJDKB-MN4SKPM-ZCUNNVL-TSFATLQ-NCLGLA4";
+        };
       };
       folders = {
         "tu" = {
           path = "/home/raphael/tu";
-          devices = [ "desktop" ];
+          devices = [ "desktop" "desktop-gentoo"];
+          versioning = {
+            type = "staggered";
+            params = {
+              cleanInterval = "3600";
+              maxAge = "2592000";
+            };
+          };
+        };
+        "documents" = {
+          path = "/home/raphael/documents";
+          devices = [ "desktop" "desktop-gentoo"];
           versioning = {
             type = "staggered";
             params = {
