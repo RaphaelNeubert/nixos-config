029fa0542b50151363ccba08604f4360d0e679ed
Raphael Neubert <raphi.neubert@gmail.com>
2024-11-10 18:29:22 +0100

    cups

--- modules/services.nix
+++ modules/services.nix
@@ -2,6 +2,11 @@
 
 {
   services.locate.enable = true;
-
   services.udev.packages = [ pkgs.qlcplus ];
+  services.printing.enable = true;
+  services.avahi = {	# for printer discovery 
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
