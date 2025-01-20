ccbc9de6be359497614acc32c6fec5d7c270f553
Raphael Neubert <raphi.neubert@gmail.com>
2025-01-20 15:09:37 +0100

    hyprland: change cursor (attempt)

--- home-modules/hyprland.nix
+++ home-modules/hyprland.nix
@@ -106,4 +106,12 @@
       };
     };
   };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    #x11.enable = true;
+    name = "capitaine-cursors";
+    package = pkgs.capitaine-cursors;
+    size = 24;
+  };
 }
