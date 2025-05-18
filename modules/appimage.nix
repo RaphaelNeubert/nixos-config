a637cbde570536b979d5ba3a6a8bf36a7affdf2f
Raphael Neubert <raphi.neubert@gmail.com>
2025-05-19 01:39:19 +0200

    add appimg-run pkgs

--- modules/appimage.nix
+++ modules/appimage.nix
@@ -6,6 +6,12 @@
     extraPkgs = pkgs: [
       pkgs.fftw
       pkgs.libsForQt5.qt5.qtwayland
+      pkgs.qt6.full
+      pkgs.qt6.qtbase
+      pkgs.qt6.qtwayland
+      pkgs.wayland
+
+      pkgs.zstd
     ];
   };
 }
