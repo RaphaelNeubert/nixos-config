16a1b8e8d8f4771264606d9f9ed3a1cbd3ab569c
Raphael Neubert <raphi.neubert@gmail.com>
2025-06-23 19:05:29 +0200

    cleanup

--- modules/appimage.nix
+++ modules/appimage.nix
@@ -5,9 +5,9 @@
   programs.appimage.package = pkgs.appimage-run.override {
     extraPkgs = pkgs: [
       pkgs.fftw
-      pkgs.libsForQt5.qt5.qtwayland
-      pkgs.qt6.full
-      pkgs.qt6.qtbase
+      #pkgs.libsForQt5.qt5.qtwayland
+      #pkgs.qt6.full
+      #pkgs.qt6.qtbase
       pkgs.qt6.qtwayland
       pkgs.wayland
 
