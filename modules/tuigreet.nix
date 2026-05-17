5d1787532dc4066a71993b604933bb26933b66e2
Raphael Neubert <raphi.neubert@gmail.com>
2026-05-17 21:19:11 +0200

    change Hyprland start command in tuigreet

--- modules/tuigreet.nix
+++ modules/tuigreet.nix
@@ -7,7 +7,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time -r --cmd Hyprland";
+        command = "${pkgs.tuigreet}/bin/tuigreet --time -r --cmd start-hyprland";
         user = "raphael";
       };
     };
